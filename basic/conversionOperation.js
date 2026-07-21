32a2a5cfe8db9dd0c151b539b8fee2e542c311ff
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-21 11:48:28 +0000

    i have learn about many conversion like number to string ,, string to number ,, null to number ,, undefined to number

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -1,3 +1,42 @@
-let score = 44
+let score1 = 44
+let score2 = "33abc"
+let score3 = null
+let score4 = undefined
 
-const {score} = req.body //we take the data from frontend and we dont know wether it is a number or string 1
+//const {score} = req.body //we take the data from frontend and we dont know wether it is a number or string 1
+
+let valueInNum1 = Number(score1)
+let valueInNum2 =Number(score2)
+let valueInNum3 = Number(score3)
+let valueInNum4= Number(score4)
+
+
+console.log(typeof(score1)); 
+console.log(typeof(score2));
+console.log(typeof(score3));
+console.log(typeof(score4));
+
+
+
+console.log(valueInNum1);
+console.log(valueInNum2);
+
+console.log(valueInNum3);
+
+console.log(valueInNum4);
+
+
+console.log(typeof(valueInNum1)); 
+console.log(typeof(valueInNum2));
+console.log(typeof(valueInNum3));
+console.log(typeof(valueInNum4));
+
+let num = 33 
+let stringNumber = String(num)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
