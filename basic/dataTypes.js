f672c9b6b37b0d90d28e73e18958c654210d8b0b
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-21 07:06:32 +0000

    learnt dataTypes

---
+++ basic/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// alert(3 + 4); //we are using nodejs not brower so it wont show alert
+
+let name = "riyaz"
+let age = 23
+let isLoggedIn = false
+
+/*
+type of data types
+number => ranges 2 to power 53
+bigint 
+string => can be written inside dual or single quot
+boolean => true/false
+null => it is stand alone value . representation of empty value and it is an object
+undefined => value is not assigned till yet
+symbol => used for unique 
+ 
+all above are premitive data types
+
+object
+ */
+console.log(typeof null);
