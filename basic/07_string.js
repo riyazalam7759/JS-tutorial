8ddfbf67ffcffc410ca2dcd7e1cf5b8a2cb644f9
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-22 06:05:28 +0000

    learning more about string like concatenation

---
+++ basic/07_string.js
@@ -0,0 +1,11 @@
+
+const name = "riyaz "
+const rollNo = 6
+console.log(name + rollNo + " sital") //concatenation
+//but use backticks `` for concatenation we use backticks cause of string interpollation
+//cause we make placeholder so that we can inject any variable with the help of ${}
+//like as for exxample 
+console.log(`my name is: ${name} and my roll Number is: ${rollNo}`);
+
+
+
