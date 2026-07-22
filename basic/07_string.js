78a70c7b4653a36fae5644db4fdb2c3f6937e2f4
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-22 06:17:30 +0000

    learn some predefined methods of string like .length , .toUpperCase etc

--- basic/07_string.js
+++ basic/07_string.js
@@ -1,11 +1,19 @@
 
-const name = "riyaz "
+const name = "riyaz "//string behind the scene this also invoked object
 const rollNo = 6
 console.log(name + rollNo + " sital") //concatenation
 //but use backticks `` for concatenation we use backticks cause of string interpollation
 //cause we make placeholder so that we can inject any variable with the help of ${}
 //like as for exxample 
-console.log(`my name is: ${name} and my roll Number is: ${rollNo}`);
+console.log(`my name is: ${name} and my roll Number is: ${rollNo}`);//called it as string interpollation
+const gameName = new String('GTriyaz')//we have used object to create string and written string inside the constructor
+console.log(gameName[0]);//=>G
+console.log(gameName.length);
+console.log(gameName.toUpperCase());//but it doesnt change the original string
+
+
+
+
 
 
 
