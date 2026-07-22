36dc629cc21f309fb48e0aed912ff80efedf26f1
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-22 05:13:24 +0000

    dataType summery

--- basic/dataTypesSummery6.js
+++ basic/dataTypesSummery6.js
@@ -1,3 +1,25 @@
 
 
 
+const id = Symbol('123')
+const  anotherId = Symbol('123')
+
+console.log(id === anotherId); //false 
+const bigNum = 482364767534875734n
+
+const heros = ["shakti" , "Ironman" , "thor"] //reference type array
+
+let myObj ={
+    name: "riyaz",
+    age: 23,
+    home:"bihar"
+} //this is an object
+
+const myFunction = function(){
+    console.log("hello this is function");
+    
+}//this is function
+
+
+console.log(typeof bigNum);
+
