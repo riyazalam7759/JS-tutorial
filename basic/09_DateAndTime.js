9d01f3a53fde6642d7e042a87a4e81b27c86cc80
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-23 07:31:26 +0000

    learned about time Stamp

--- basic/09_DateAndTime.js
+++ basic/09_DateAndTime.js
@@ -9,6 +9,26 @@ console.log(myDate.toISOString());
 console.log(myDate.toJSON());
 console.log(typeof myDate);//=>this is an oject
 
+let myCreatedDate = new Date(2023 , 0 ,24 , 5,3)//in JavaScript month start from 0 zero
+console.log(myCreatedDate.toLocaleString());//i have created this date 
+
+let myCreatedDate2 = new Date("2023-01-14")//another syntax of Date
+console.log(myCreatedDate2.toLocaleString());
+
+//++++++++++++++++++++++++++++++++TIME STAMPS+++++++++++++++++++++++++++++++++
+
+console.log("\n\nLEARNING ABOUT TIME STAMP");
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);//give the time in milisecond
+console.log(Math.floor(Date.now()/1000));//changes into second
+
+
+
+
+
+
+
 
 
 
