ef6dcf7486543ce91e45c89efe83f50a9d871c33
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-23 05:37:25 +0000

    learning about some methods of number

--- basic/08_NumsAndMaths.js
+++ basic/08_NumsAndMaths.js
@@ -1,8 +1,22 @@
-
+//numbers And Maths 2:30:00
  const score = 400
  console.log(score);
 
  const balance = new Number(100)
  console.log(balance);
  
+ console.log(balance.toString());//converted into string 
+ console.log(balance.toFixed(2));//go to the 2 precision after decimal
+ 
+const otherNum = 12.8966//means doing roundoff
+console.log(otherNum.toPrecision(3));//give the precise value contain 3 digit including after decimal or overall digits
+
+
+
+
+
+
+
+
+
  
