1677d15b4df161ff8c3de1f90802fe0f7d9d131d
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-23 06:44:41 +0000

    start learning about Date and Time in JavaScript

---
+++ basic/09_DateAndTime.js
@@ -0,0 +1,18 @@
+//Date and Time in JS =>2:54:00
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(typeof myDate);//=>this is an oject
+
+
+
+
+
+
+
+
