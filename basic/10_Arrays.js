2240f47aa9b15c284eb1941f9a1c3c60881c0565
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-24 05:07:56 +0000

    basic about array

--- basic/10_Arrays.js
+++ basic/10_Arrays.js
@@ -1,6 +1,24 @@
 // ARRAYS => 3:12:00
 
 const myArr=[0,1,2,3,4,5]
+//array in JS can be resized and can store different type of element also in same array 
+//array can have array inside array
+//whenever we do copy operation then it makes shallow copy not deep copy
+//means shares the same reference if i will change in the copy array then there will also be changed in original array 
+
+const myHeros = ["shakti" , "ironMan" , "captain"]
+const myArr2 = new Array(1,2,3,4)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
