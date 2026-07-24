a7941a38c938823b253df52724627cd4bfdfd0fe
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-24 14:13:09 +0000

    learned about freeze that does not make to do any changes in  the object

--- basic/12_Objects.js
+++ basic/12_Objects.js
@@ -7,9 +7,14 @@
 
 //+++++++OBJECT LITERALS+++++++++
 
+const mySym = Symbol("key1") //declared symbol then how to use as key in object
+const mySym2 = Symbol("key2")
 const JsUser = {//creating object as lliterals which contains keys and values
 
     name:"Riyaz",//name is here as string too
+    "Full Name":"Riyaz Rafique",//we cant access this fullName using dot .
+    mySym:"MyKey1",
+    [mySym2]:"myKey2",
     age:"23",
     location:"Hyderabad",
     email:"riyaz@gmail.com",
@@ -20,8 +25,15 @@ const JsUser = {//creating object as lliterals which contains keys and values
 //we can access all element in object by dot . like JsUser.email etc
 console.log("A" ,JsUser.email);
 console.log("B" ,JsUser["email"])//behind the scene name email kept as string so we have to give here as string too
+console.log("C" ,JsUser["Full Name"]);
+console.log("D" ,JsUser.mySym);//it will give the value but datatype will not be symbol datatype will be string here
+console.log("E" , JsUser[mySym2]);//now datatype will be symbol
 
-
+console.log("\n\nCHANGING VALUE OF KEY OF OBJECT");
+JsUser.email="23btsc006@gmail.com"
+Object.freeze(JsUser)//now object get freezed and we cant make changes in object JsUser
+JsUser.email="23bt@gmail.com"//now email wont be changed
+console.log(JsUser);
 
 
 
