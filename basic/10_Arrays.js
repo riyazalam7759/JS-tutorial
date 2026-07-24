b0fde36646da03feab612ef81ee0d34bae0b488e
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-24 05:39:18 +0000

    learned about slice, splice

--- basic/10_Arrays.js
+++ basic/10_Arrays.js
@@ -1,6 +1,6 @@
 // ARRAYS => 3:12:00
 
-const myArr=[0,1,2,3,4,5]
+const myArr=[10,11,12,13,14,15]
 //array in JS can be resized and can store different type of element also in same array 
 //array can have array inside array
 //whenever we do copy operation then it makes shallow copy not deep copy
@@ -23,6 +23,19 @@ console.log(myArr.includes(12))//is there any element of 12 value and give outpu
 
 console.log(myArr.indexOf(3));//=>findig index of element 3
 
+const newArr = myArr.join()//it will add all the element of array in string and separated by specified separator
+console.log(myArr);
+console.log(newArr);//and type will be changes into string 
+
+console.log("A " , myArr);
+const myArr3 = myArr.slice(1 ,4)//extract part of the array from indes 1 to 4
+console.log(myArr3);
+
+console.log("B " ,myArr);
+const myArr4 = myArr.splice(1,4)//this will change whole array and will be there only ranges values in main array 
+console.log(myArr4);
+
+console.log("C ",myArr);
 
 
 
