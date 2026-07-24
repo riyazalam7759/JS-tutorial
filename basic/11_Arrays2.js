448e704df1a89f0e93d88f0cf1ee49d9a3bd1604
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-24 13:11:43 +0000

    learn more about arrays like ,flat ,isArray ,from ,and of

--- basic/11_Arrays2.js
+++ basic/11_Arrays2.js
@@ -11,4 +11,27 @@ console.log("All heros :" ,AllHeros);
 const AllHeros2 = [ ...marvel , ...dc]//...marvel =>this will spread every element of the array..also we can add more arrays
 console.log(AllHeros2);//now each element of both arrays will be counted as separaate element of this Allheros2
 
+const arr1 = [21,22,23,[24,25,26],27,[28,29,[30,31]]]
+const flatArr=arr1.flat(Infinity)//in place of infinity we can give the level of array to be flatted
+console.log(flatArr);
+
+console.log(Array.isArray("riyazz"));//it give output as true or false is riyazz array or not 
+console.log(Array.from("riyazz"));//this is string converted into array with each alphabet as element we can give anything like object etc to convert into array 
+console.log(Array.from({name:"Riyaz"}));//=>it will give empty array you have to tell whether make array of kays or values 
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of);//it will return the new array from the set of element it may variable , arrays etc
+
+
+
+
+
+
+
+
+
+
 
