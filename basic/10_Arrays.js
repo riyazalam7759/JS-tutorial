615a24a25266504f3cbe321b867f7bbd3e65fefd
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-24 05:23:17 +0000

    learned some basic method about arrays like shift ,unshift , include and etc

--- basic/10_Arrays.js
+++ basic/10_Arrays.js
@@ -8,6 +8,24 @@ const myArr=[0,1,2,3,4,5]
 
 const myHeros = ["shakti" , "ironMan" , "captain"]
 const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+//Array Methods 
+console.log("SOME BASIC METHOD ABOUT ARRAY ");
+myArr.push(6)
+myArr.push(7)//added element at last of the array 
+console.log(myArr);
+myArr.pop()//it will delete the last element of the array 
+myArr.unshift(8)//added element at the starting of the array then all the previous element will be shifted toward
+myArr.shift()//this will remove the first element of the array
+
+console.log(myArr.includes(12))//is there any element of 12 value and give output in true or false 
+
+console.log(myArr.indexOf(3));//=>findig index of element 3
+
+
+
+
 
 
 
