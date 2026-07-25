f74943a2d7a08de002b54739288fb57d5ae832c4
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-25 11:37:36 +0000

    basic about function

--- function/01_functions.js
+++ function/01_functions.js
@@ -1,4 +1,23 @@
+//function => 4:40:00
+function sayMyName(){
+    console.log("r");
+    console.log("i");
+    console.log("y");
+    console.log("a");
+     
+}
+sayMyName//=>this is a reference of the function it show here function live
+sayMyName()
 
+function addTwoNumbers(n1,n2){//parameters
+    console.log(n1+n2)
+}
 
+addTwoNumbers(3,4)//arguments
 
-
+function addTwoNumbers2(n1,n2){
+    let result = n1+n2
+    return result
+}
+let res = addTwoNumbers2(4,5)
+console.log(res)
