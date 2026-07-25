2c3e4bf0f950294fa8c3eb685623122494a6cf16
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-25 06:09:27 +0000

    learn about method like assign, Object.keys , Object.values , Object.enteries , hasOwnProperty

--- basic/13_Objects2.js
+++ basic/13_Objects2.js
@@ -34,5 +34,28 @@ const obj4=Object.assign({},obj1,obj2,obj3)//=>syntax to combine two object here
 console.log("E",obj4)//object assign is a static method to copy all one or more source objects to form a target object and it returns the target modified object 
 //Object.assign(target , source1 , source2 , more) so we add an empty object 
 
+const obj5={...obj1,...obj2}//spreading out the objects
+console.log(obj5);
+console.log("F",Object.keys(obj2))//=>output will be as arrays
+console.log("G",Object.values(obj2))//give the values of object in form of arrays
+console.log("H",Object.entries(obj2))//it will make arrays of arrays each array has two element keys and values and these arrays will be in a outside array
 
+console.log(obj2.hasOwnProperty('9'))//does this obj2 contain 9(check key only) =>false
+console.log(obj2.hasOwnProperty('5'))
+
+const user=[//objecct inside array
+    {
+        name:"riyaz",
+        email:"ryz@gmail.com"
+    },
+    {
+        name:"Faiz",
+        email:"fz@gmail.com"
+    },
+    {
+        name:"jani",
+        email:"jani@gmail.com"
+    }
+]
+console.log(user[1].email)//=> output will be fz@gmail.com
 
