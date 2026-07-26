d038766c41b675287f640bbbdd1d9c8a8625da2c
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-26 07:05:38 +0000

    seen two type of declaration of function

--- function/02_scope.js
+++ function/02_scope.js
@@ -11,6 +11,29 @@ console.log("global a:" ,a);//global a will be printed
 //console.log(b)=>wont be printed bcause b is executable only inside the if condition 
 console.log(c)//=> but c is a global 
 
+function one(){
+    const username ="riyaz"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    two()
+    //console.log(website)//=>website will not be printed 
+}
+one()//=> this all above is called closure
+
+//++++++++++++++++++++++++interesting++++++++++++++++++++++
+
+console.log(addOne(3))//this will run
+function addOne(num){
+    return num + 1
+}
+
+//addTwo(3)//=>this will give the error it depends on how you are declaring a function
+const addTwo = function(num){//this is as variable sometimes called expression
+    return num + 2
+}
+
 
 
 
