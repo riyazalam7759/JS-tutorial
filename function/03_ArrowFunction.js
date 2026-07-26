64e1542a4bf5c66ef5b05a5c6b2b46995b869cf9
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-26 11:39:04 +0000

    learn about this keyword where it will show many value and where it will show undefined inside function and where it will show  current context

--- function/03_ArrowFunction.js
+++ function/03_ArrowFunction.js
@@ -16,6 +16,29 @@
  user.welcomeMessage()
  console.log("outside THIS" , this)//=> it will give the output empty object cause there is nothing in the current context
 
+console.log("\n\nMORE ABOUT THIS KEYWORD")
+function chai(){
+    let username ="hitesh"
+    console.log(this.username)//it will show undefined this context will only work iside object not in function
+    //console.log(this)//many values will be shown 
+}
+chai()
+
+const chai2 =function(){
+    let username2="riyaxx"
+    console.log(this.username2)//it will also show undefined
+    //console.log(this) many value will be shown 
+}
+chai2()
+
+console.log("\n\n NOW STARTED ARROW FUNCTION")
+
+const chai3 =()=>{
+    let username3 = "faiz"
+    console.log(this.username3)//there will be also undefined
+    console.log(this)//=> empty object
+}
+chai3()
 
 
 
