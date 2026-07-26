5a7bef0b57aa49082c2481cce6aa8039a1b3a600
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-26 17:08:45 +0000

    making hand notes in this topic

---
+++ function/05_JEC.js
@@ -0,0 +1,14 @@
+//JAVASCRIPT EXECUTION CONTEXT 5:58:00
+//How JavaScript execute files and how it runs
+//first divided into two phase 
+//there will be Global Execution Context with execution context there will be this keyword 
+//and the value of this keyword is window object in the browser
+//this global execution context is executed inside thread
+//JavaScript is a single threaded
+
+
+
+
+
+
+
