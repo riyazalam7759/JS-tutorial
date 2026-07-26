0cf3b098050309956810e6f0c55b5a4791da20d2
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-26 11:18:57 +0000

    know more about this keyword

--- function/03_ArrowFunction.js
+++ function/03_ArrowFunction.js
@@ -1,3 +1,24 @@
+//ARROW FUNCTIONS 5:30:00
+ //this keyword tell us about the current context or it refer current context
+//inside browser most of the global object are window object 
+ const user ={
+    username:"Riyaz",
+    price:999,
+    welcomeMessage: function(){
+        console.log(`${this.username} welcome to website`)//here you have to add this keyword 
+        console.log(this);//output will be all the current context
+        
+    }
+ }
+
+ user.welcomeMessage()
+ user.username="sam"
+ user.welcomeMessage()
+ console.log("outside THIS" , this)//=> it will give the output empty object cause there is nothing in the current context
+
+
+
+
 
 
 
