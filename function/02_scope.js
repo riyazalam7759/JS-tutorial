6cdaada1f558e8af29435a69d87bbca5aff31ec6
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-26 06:25:53 +0000

    learned about global and local scope

--- function/02_scope.js
+++ function/02_scope.js
@@ -1,3 +1,15 @@
+//SCOPE => 5:6:00
+let a = 100
+if(true)
+{
+    let a=10
+    const b=20
+    var c=30
+    console.log("inner a :",a)
+}
+console.log("global a:" ,a);//global a will be printed 
+//console.log(b)=>wont be printed bcause b is executable only inside the if condition 
+console.log(c)//=> but c is a global 
 
 
 
