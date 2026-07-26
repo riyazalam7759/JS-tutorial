0fc76fa8dec1e375fb44a40424e1041642fb6a5c
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-26 12:30:42 +0000

    know about IIFE of arrow function

--- function/04_ArrowFunction2.js
+++ function/04_ArrowFunction2.js
@@ -8,4 +8,33 @@ const addTwo = (num1 , num2)=>{
 console.log(addTwo(3,4))
 
 const ReturnObject = (n1 , n2)=>({username:"ryz"})//for returning object you have to write paranthesis()
-console.log(ReturnObject(1,2))
+console.log(ReturnObject(1,2));
+
+//++++++Immediate Invoked Function Expression (IIFE)+++++++
+//we want immediate execution of function
+//before making arrow function add a semi colon ;
+
+(function chai(){//this is a named IIFE
+    console.log(`DB connected`);
+})();
+
+(()=>{
+    console.log(`DB connected two`)
+})();
+
+(function func(){
+    console.log(`DB connected three`)
+})();
+
+((name)=>{//simple or unnamed IFFE
+    console.log(`DB connected four ${name}`)
+})('Riyazz');//argument given to the arrow function
+
+
+
+
+
+
+
+
+
