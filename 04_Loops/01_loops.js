c827e4c18a2066379eadd672521c707ec39f6e4f
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-27 11:31:38 +0000

    know basic about for loop it is same as the c++ for loop and there is also concept of break and continue

---
+++ 04_Loops/01_loops.js
@@ -0,0 +1,15 @@
+
+//LOOPS => 7:17:00
+//FOR LOOPS 
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+    
+}
+//for loop is same the c++ for loop
+//there is also concept of continue and break like c++
+
+
+
+
+
