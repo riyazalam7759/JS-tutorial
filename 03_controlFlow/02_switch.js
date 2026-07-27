2dc01c2b5f5146c7278f4168c8bd57f33d210867
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-27 06:29:01 +0000

    basic switch

---
+++ 03_controlFlow/02_switch.js
@@ -0,0 +1,21 @@
+// switch case => 6:47:00
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("mar")
+        break;
+    case 4:
+        console.log("apr")
+        break;
+
+    default:
+        console.log(" default case none ")
+        break;
+}
