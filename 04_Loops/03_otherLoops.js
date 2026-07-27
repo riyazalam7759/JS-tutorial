df573cc2e1a160edb4d52ad608fa9e8a399089b1
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-27 12:32:24 +0000

    know about for of loop

---
+++ 04_Loops/03_otherLoops.js
@@ -0,0 +1,17 @@
+//other loops =>7:50:00
+//these are arrays specific loops
+
+const arr = [10 , 11 , 12 , 13 , 14]
+// for (const element of object) {   syntax of for of loop
+    
+// }
+
+for (const num of arr) {
+    console.log(num)  
+}
+
+const greeting = "hello world"
+for(const greet of greeting){
+    console.log(`each character ${greet}`)
+}
+
