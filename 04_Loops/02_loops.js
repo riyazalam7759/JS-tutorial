d44b0d0e896d2ce2f863fa903788c5d1a543761d
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-07-27 11:42:55 +0000

    learned about while and do while loops which are same as the c++ do while and while loops

---
+++ 04_Loops/02_loops.js
@@ -0,0 +1,15 @@
+// Do and Do while loops =>7:40:00
+//while loop is same as the c++ while loop
+let i = 0
+while (i<=10) {
+    console.log(`value of i is ${i}`)
+    i += 2
+}
+console.log("\n\n")
+
+//do while loop atleast one time will run 
+let score = 1
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <=10);
